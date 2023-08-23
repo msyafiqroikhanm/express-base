d5612bce02a7caf0ee66326b76df580ee6a83d31
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-08-23 15:28:35 +0700

    ♻️ Refacator Error Handling And Validate Middleware

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -1,5 +1,7 @@
+const { relative } = require('path');
 const { validationResult } = require('express-validator');
 const ResponseFormatter = require('../helpers/responseFormatter.helper');
+const deleteFile = require('../helpers/deleteFile.helper');
 
 class ValidateMiddleware {
   static async result(req, res, next) {
@@ -11,7 +13,34 @@ class ValidateMiddleware {
         errors.errors.forEach((element) => {
           resErrors.push(element.msg);
         });
-        return ResponseFormatter.error400(res, 'Data tidak lengkap', resErrors);
+        return ResponseFormatter.error400(res, 'Data Not Complete', resErrors);
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async resultWithMandatoryFile(req, res, next) {
+    try {
+      res.url = req.originalUrl;
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty() || !req.file) {
+        const resErrors = [];
+        errors.errors.forEach((element) => {
+          resErrors.push(element.msg);
+        });
+
+        if (!req.file) {
+          resErrors.push('Missing Mandatory File, File Required');
+        }
+
+        // Delete uploaded file when error happens
+        if (req.file) {
+          await deleteFile(relative(__dirname, req.file.path));
+        }
+        return ResponseFormatter.error400(res, 'Data Not Complete', resErrors);
       }
       next();
     } catch (error) {
