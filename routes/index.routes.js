64da74ea0ca2e379b35db6cb94ca0ef06c763c8b
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-08-23 18:09:35 +0700

    ➕ Create QR Endpoint (Route, Controller, Service)

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,11 +4,13 @@ const authRoutes = require('./auth.routes');
 const configRoutes = require('./sysConfiguration.routes');
 const referenceRoutes = require('./reference.routes');
 const qrTemplateRoutes = require('./qrTemplate.routes');
+const qrRoutes = require('./qr.routes');
 
 router.get('/', (req, res) => res.send(`${process.env.APP_NAME} App `));
 router.use('/auth', authRoutes);
 router.use('/config', configRoutes);
 router.use('/reference', referenceRoutes);
 router.use('/qr-templates', qrTemplateRoutes);
+router.use('/qrs', qrRoutes);
 
 module.exports = router;
