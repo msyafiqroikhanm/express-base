7835420c49d73869d4cb14b2d6ce4509f25c25ce
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-08-28 11:02:15 +0700

    ♻️ Refactor Auth Middleware ------------------------------------------------------------------------ Now check if user login and has access to feature at the same time or one middleware method

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,9 +1,10 @@
 const passport = require('passport');
 
 class AuthMiddleware {
-  static async authenticate(req, res, next) {
+  static async authenticate(req, res, next, requiredFeatures) {
     try {
       passport.authenticate('jwt', { session: false }, (err, userData, info) => {
+        // authenticate if user have login or not
         if (err) {
           return next(err);
         }
@@ -16,27 +17,20 @@ class AuthMiddleware {
           throw { code: 401, status: 'Unauthorized Request', message: 'Please login to access this feature' };
         }
         req.user = userData;
+
+        // check user access
+        const authorized = req.user.Role.USR_Features
+          .some((feature) => requiredFeatures.includes(feature.id));
+
+        if (!authorized) {
+          throw {
+            code: 401,
+            status: 'Unauthorized Request',
+            message: 'You Don\'t Have Access To This Feature',
+          };
+        }
         return next();
       })(req, res, next);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async authorization(req, res, next, requiredFeature) {
-    try {
-      const authorized = req.user.Role.USR_Features
-        .some((feature) => feature.id === requiredFeature);
-
-      if (!authorized) {
-        throw {
-          code: 401,
-          status: 'Unauthorized Request',
-          message: 'Anda tidak memiliki akses ke layanan ini',
-        };
-      }
-
-      next();
     } catch (error) {
       next(error);
     }
