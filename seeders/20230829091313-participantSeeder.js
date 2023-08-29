861c8d3b13f06831f80c3645e982c576511bad94
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-08-29 23:29:00 +0700

    ➕ Create Seed File ------------------------------------------------------------------------ - REF_Regions - REF_GroupStatuses - REF_ParticipantTypes - REF_IdentityTypes - PAR_Contingents - PAR_ContingentGroups

---
+++ seeders/20230829091313-participantSeeder.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+
+'use strict';
+
+const fs = require('fs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    //* PAR_Contingents
+    const par_contingets = JSON.parse(
+      fs.readFileSync('./seeders/data/par_contingents.json'),
+    );
+
+    const identityTypes = par_contingets.map((element) => ({
+      regionId: element.regionId,
+      name: element.name,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+    await queryInterface.bulkInsert('PAR_Contingents', identityTypes);
+
+    //* PAR_ContingentGroups
+    const par_contingentgroups = JSON.parse(
+      fs.readFileSync('./seeders/data/par_contingentgroups.json'),
+    );
+
+    const contingentGroups = par_contingentgroups.map((element) => ({
+      eventId: element.eventId,
+      contingentId: element.contingentId,
+      statusId: element.statusId,
+      name: element.name,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+    await queryInterface.bulkInsert('PAR_ContingentGroups', contingentGroups);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('PAR_Contingents', null, {
+      truncate: true,
+      restartIdentity: true,
+    });
+    await queryInterface.bulkDelete('PAR_ContingentGroups', null, {
+      truncate: true,
+      restartIdentity: true,
+    });
+  },
+};
