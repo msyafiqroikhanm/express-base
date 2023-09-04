ff6d44913a05dd599eb4007d0a9ef51937f367ab
Syafiq <msyafiq.roikhanm@gmail.com>
2023-09-04 11:05:02 +0700

    ➕Create Model and Migration ACM_ParticipantLodgers

---
+++ models/acm_participantlodger.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ACM_ParticipantLodger extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ACM_ParticipantLodger.init({
+    participantId: DataTypes.INTEGER,
+    statusId: DataTypes.INTEGER,
+    roomId: DataTypes.INTEGER,
+    reservationIn: DataTypes.DATEONLY,
+    reservationOut: DataTypes.DATEONLY,
+    checkIn: DataTypes.DATE,
+    checkout: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'ACM_ParticipantLodger',
+  });
+  return ACM_ParticipantLodger;
+};
