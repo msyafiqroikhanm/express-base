4c5ad835ab951e27696ea20f97d43ef43aa500ac
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-09-14 16:38:02 +0700

    ➕ Create Model, Migration, Seed Ref committee Type

---
+++ models/ref_committeetype.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class REF_CommitteeType extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      REF_CommitteeType.hasMany(models.PAR_Participant, { foreignKey: 'committteeTypeId', as: 'committees' });
+    }
+  }
+  REF_CommitteeType.init({
+    name: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'REF_CommitteeType',
+  });
+  return REF_CommitteeType;
+};
