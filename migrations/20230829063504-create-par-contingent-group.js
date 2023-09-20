c94ff596018ea039f3ab7f295f6cf3178b7a489b
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-09-20 10:54:54 +0700

    ♻️ Refactor Contingent And Group To Paranoid

--- migrations/20230829063504-create-par-contingent-group.js
+++ migrations/20230829063504-create-par-contingent-group.js
@@ -5,34 +5,43 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('PAR_Groups', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      'PAR_Groups',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        eventId: {
+          type: Sequelize.INTEGER,
+        },
+        contingentId: {
+          type: Sequelize.INTEGER,
+        },
+        statusId: {
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        deletedAt: {
+          type: Sequelize.DATE,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      eventId: {
-        type: Sequelize.INTEGER,
+      {
+        paranoid: true,
       },
-      contingentId: {
-        type: Sequelize.INTEGER,
-      },
-      statusId: {
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('PAR_Groups');
