a4aed3e1a2e59a342a475516a179f40f49ab862d
Syafiq <msyafiq.roikhanm@gmail.com>
2023-09-25 11:34:16 +0700

    ♻️ Update event seeder

--- seeders/20230828063338-EventSeeder.js
+++ seeders/20230828063338-EventSeeder.js
@@ -8,23 +8,22 @@ const fs = require('fs');
 module.exports = {
   async up(queryInterface, Sequelize) {
     //* ENV_Events
-    const env_events = JSON.parse(
-      fs.readFileSync('./seeders/data/env_events.json'),
-    );
+    const env_events = JSON.parse(fs.readFileSync('./seeders/data/env_events.json'));
     const events = env_events.map((element) => ({
       picId: element.picId,
       categoryId: element.categoryId,
       locationId: element.locationId,
       name: element.name,
+      minAge: element.minAge,
+      maxAge: element.maxAge,
+      maxParticipantPerGroup: element.maxParticipantPerGroup,
       createdAt: new Date(),
       updatedAt: new Date(),
     }));
     await queryInterface.bulkInsert('ENV_Events', events);
 
     //* ENV_TimeEvents
-    const env_timeevent = JSON.parse(
-      fs.readFileSync('./seeders/data/env_timeevents.json'),
-    );
+    const env_timeevent = JSON.parse(fs.readFileSync('./seeders/data/env_timeevents.json'));
     const timeEvents = env_timeevent.map((element) => ({
       eventId: element.eventId,
       start: new Date(element.start),
