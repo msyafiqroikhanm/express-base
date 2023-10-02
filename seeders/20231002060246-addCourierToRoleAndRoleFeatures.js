ebe93d5bbe93db17a8b14d6d28614aa549b82071
Syafiq <msyafiq.roikhanm@gmail.com>
2023-10-02 13:29:06 +0700

    ➕Create new seeder for Add Courier To Role and RoleFeature

---
+++ seeders/20231002060246-addCourierToRoleAndRoleFeatures.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+
+('use strict');
+
+const fs = require('fs');
+const { USR_Role } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const courierRoleInstance = await USR_Role.findOne({ where: { name: 'Courier' } });
+    if (!courierRoleInstance) {
+      await queryInterface.bulkInsert('USR_Roles', [
+        {
+          templateId: 1,
+          name: 'Courier',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+    }
+
+    const courierRoleFeatures = JSON.parse(
+      fs.readFileSync('./seeders/data/usr_rolefeatures_courier.json'),
+    );
+    const responses = courierRoleFeatures.map((element) => ({
+      roleId: courierRoleInstance.id,
+      featureId: element.featureId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+    await queryInterface.bulkInsert('USR_RoleFeatures', responses);
+  },
+
+  async down(queryInterface, Sequelize) {
+    const courierRoleInstance = await USR_Role.findOne({ where: { name: 'Courier' } });
+    await queryInterface.bulkDelete('USR_Roles', { name: 'Courier' }, null);
+    await queryInterface.bulkDelete('USR_RoleFeatures', { roleId: courierRoleInstance.id }, null);
+  },
+};
