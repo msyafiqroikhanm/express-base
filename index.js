9f69ad5c3b492ba009182b251d5d3ec57349b27a
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-10-02 14:17:50 +0700

    ➕ Create Simple Websocket For Barebone Notification

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const http = require('http');
 const app = require('./app');
 const db = require('./models');
 const { metaBroadcastSynchronize } = require('./config/cron');
+const { initializeSocketIO } = require('./config/websocket');
 
 const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const server = http.createServer(app);
+const io = initializeSocketIO(server);
+app.set('socketIo', io);
 server.listen(port, async () => {
   await db.sequelize.authenticate();
   console.log(`${process.env.APP_NAME} running on ${process.env.BASE_URL}`);
